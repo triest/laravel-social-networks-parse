ec247ff189f83e5de9f91297dcdf448b0e1ed448
triest <triest21@gmail.com>
2022-05-03 17:16:30 +0300

    initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="text-center" style="margin: 20px 0px 20px 0px;">
+      <span class="text-secondary">Laravel Vue CRUD Example</span>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <div class="navbar-nav">
+
+        </div>
+      </div>
+    </nav>
+    <br/>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {}
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,8 @@
+
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: home
+    },
+];
